fb566bc55eb8df8482442b3a7c48eb90f32726ba
naimuddin94 <developernaimuddin@gmail.com>
2025-01-16 13:41:17 +0600

    next ui setup

---
+++ frontend/app/providers.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import type { ThemeProviderProps } from "next-themes";
+
+import { NextUIProvider } from "@nextui-org/system";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useRouter } from "next/navigation";
+import * as React from "react";
+
+export interface ProvidersProps {
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
+}
+
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>["push"]>[1]
+    >;
+  }
+}
+
+const queryClient = new QueryClient();
+
+export function Providers({ children, themeProps }: ProvidersProps) {
+  const router = useRouter();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider navigate={router.push}>
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      </NextUIProvider>
+    </QueryClientProvider>
+  );
+}
