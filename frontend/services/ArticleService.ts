bacea72bd8337bd2fdd1c551f19253b94a42ccba
naimuddin94 <developernaimuddin@gmail.com>
2025-01-24 22:45:14 +0600

    fetch single article

--- frontend/services/ArticleService.ts
+++ frontend/services/ArticleService.ts
@@ -5,8 +5,12 @@
 import { FieldValues } from "react-hook-form";
 
 import axiosInstance from "@/lib/axiosInstance";
-import { IArticle, IFilterOptions, IResponseWithMetadata } from "@/types";
-import { wait } from "@/utils/wait";
+import {
+  IArticle,
+  IFilterOptions,
+  IResponse,
+  IResponseWithMetadata,
+} from "@/types";
 
 export const createArticle = async (articleData: FieldValues) => {
   try {
@@ -22,12 +26,23 @@ export const fetchArticles = async (
   params: IFilterOptions,
 ): Promise<IResponseWithMetadata<IArticle[]>> => {
   try {
-    await wait();
     // Build query string dynamically
     const queryParams = new URLSearchParams(
       Object.entries(params).filter(([_, value]) => value !== undefined),
     ).toString();
     const { data } = await axiosInstance.get(`/articles?${queryParams}`);
+
+    return data;
+  } catch (error: any) {
+    throw new Error(error?.response?.data?.message);
+  }
+};
+
+export const fetchArticle = async (
+  articleId: string,
+): Promise<IResponse<IArticle>> => {
+  try {
+    const { data } = await axiosInstance.get(`/articles/${articleId}`);
 
     return data;
   } catch (error: any) {
